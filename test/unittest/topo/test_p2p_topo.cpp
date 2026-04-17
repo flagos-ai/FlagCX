6b0612aadbb6f45c0a5da4258958d9089b0f5564
mikethegoblin <mikegu0508@gmail.com>
2026-04-17 11:01:55 +0800

    test p2p topo manager using ibrc p2p adaptor

--- test/unittest/topo/test_p2p_topo.cpp
+++ test/unittest/topo/test_p2p_topo.cpp
@@ -13,7 +13,7 @@
 #include "flagcx.h"
 #include "p2p_topo.h"
 
-extern struct flagcxNetAdaptor flagcxNetIb;
+extern struct flagcxNetAdaptor flagcxNetIbP2p;
 
 class P2pTopoTest : public ::testing::Test {
 protected:
@@ -21,13 +21,13 @@ protected:
     // Initialize device adaptor (sets global deviceAdaptor)
     flagcxHandleInit(&handler);
 
-    netAdaptor = &flagcxNetIb;
+    netAdaptor = &flagcxNetIbP2p;
     ASSERT_EQ(netAdaptor->init(), flagcxSuccess)
-        << "IB net adaptor init failed";
+        << "IB P2P net adaptor init failed";
 
     int ndev = 0;
     ASSERT_EQ(netAdaptor->devices(&ndev), flagcxSuccess);
-    ASSERT_GT(ndev, 0) << "No IB devices found";
+    ASSERT_GT(ndev, 0) << "No IB P2P devices found";
     std::cout << "Net adaptor: " << netAdaptor->name << " (" << ndev
               << " devices)" << std::endl;
   }
